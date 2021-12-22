8bf226e03017a8ec7197da50429e18bf738b40b2
EhoodGoren <ehoodgoren@gmail.com>
2021-12-23 01:38:19 +0200

    Fixed URL

--- src/redux/actions/weatherActions.js
+++ src/redux/actions/weatherActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getCityWeather = (name, apiKey) => async dispatch => {
     try {
         const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}&units=metric`);
-        const weatherIcon = await axios.get(`http://openweathermap.org/img/wn/${weatherData.data.weather[0].icon}@2x.png`, { responseType: 'blob'});
+        const weatherIcon = await axios.get(`https://openweathermap.org/img/wn/${weatherData.data.weather[0].icon}@2x.png`, { responseType: 'blob'});
         const iconSrc = URL.createObjectURL(weatherIcon.data);
         dispatch({ type: 'FOUND_CITY', weather: weatherData, iconSrc });
     } catch(error) {
